45d46b991b29f1259af51160c12dd5b5f8208fd8
Amirhossein Bozorgiyan <amirhossein@bozorgiyan.ir>
2019-08-31 17:47:56 +0430

    Add debian base install bind9

--- bindbot.py
+++ bindbot.py
@@ -2,12 +2,22 @@
 import os
 
 ## Functions
+def installDebian():
+    # Install packages
+    os.system("sudo apt install bind9 -y > /dev/null")
+    # Creat bind9 install verify file
+    os.system("sudo touch /usr/share/bindbot/.bind9")
+    # Save service name
+    os.system("sudo echo 'bind9' | sudo tee -a /usr/share/bindbot/.service > /dev/null")
+    # Save config path
+    os.system("sudo echo '/etc/bind/' | sudo tee -a /usr/share/bindbot/.etc > /dev/null")
+
 def installBind9():
     debian = os.path.exists("/bin/apt")
     redhat = os.path.exists("/bin/yum")
     if debian:
         ## Install Debian
-        pass
+        installDebian()
     elif redhat:
         ## Install Redhat
         pass
