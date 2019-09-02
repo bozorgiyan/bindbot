d1d1532766a9f4cb25fbc7eb3ee745fb3ffd4e71
Amirhossein Bozorgiyan <amirhossein@bozorgiyan.ir>
2019-09-02 15:37:16 +0430

    Add root check and remove sudo from all system.os.

--- bindbot.py
+++ bindbot.py
@@ -12,43 +12,43 @@ class bcolors:
 ## Functions
 def installDebian():
     # Install packages
-    os.system("sudo apt install bind9 -y > /dev/null 2>&1")
+    os.system("apt install bind9 -y > /dev/null 2>&1")
     # Creat bind9 install verify file
-    os.system("sudo touch /usr/share/bindbot/.bind9")
+    os.system("touch /usr/share/bindbot/.bind9")
     # Save service name
-    os.system("sudo echo 'bind9' | sudo tee -a /usr/share/bindbot/.service > /dev/null 2>&1")
+    os.system("echo 'bind9' | sudo tee -a /usr/share/bindbot/.service > /dev/null 2>&1")
     # Save config path
-    os.system("sudo echo '/etc/bind/' | sudo tee -a /usr/share/bindbot/.etc > /dev/null 2>&1")
+    os.system("echo '/etc/bind/' | sudo tee -a /usr/share/bindbot/.etc > /dev/null 2>&1")
     # Open ports
     print(bcolors.BOLD + "Opening ports..." + bcolors.ENDC)
-    os.system("sudo ufw allow 53/tcp > /dev/null 2>&1")
-    os.system("sudo ufw allow 53/udp > /dev/null 2>&1")
-    os.system("sudo iptables --append INPUT --match tcp --protocol tcp --sport 53 --jump ACCEPT > /dev/null 2>&1")
-    os.system("sudo iptables --append OUTPUT --match tcp --protocol tcp --sport 53 --jump ACCEPT > /dev/null 2>&1")
-    os.system("sudo iptables --append INPUT --match udp --protocol udp --sport 53 --jump ACCEPT > /dev/null 2>&1")
-    os.system("sudo iptables --append OUTPUT --match udp --protocol udp --sport 53 --jump ACCEPT > /dev/null 2>&1")
+    os.system("ufw allow 53/tcp > /dev/null 2>&1")
+    os.system("ufw allow 53/udp > /dev/null 2>&1")
+    os.system("iptables --append INPUT --match tcp --protocol tcp --sport 53 --jump ACCEPT > /dev/null 2>&1")
+    os.system("iptables --append OUTPUT --match tcp --protocol tcp --sport 53 --jump ACCEPT > /dev/null 2>&1")
+    os.system("iptables --append INPUT --match udp --protocol udp --sport 53 --jump ACCEPT > /dev/null 2>&1")
+    os.system("iptables --append OUTPUT --match udp --protocol udp --sport 53 --jump ACCEPT > /dev/null 2>&1")
     # Enable service
     print(bcolors.BOLD + "Enable bind9 service..." + bcolors.ENDC)
-    os.system("sudo systemctl enable bind9 > /dev/null 2>&1")
+    os.system("systemctl enable bind9 > /dev/null 2>&1")
 
     print(bcolors.OKGREEN + "Bind9 installation finished." + bcolors.ENDC)
 def installRedhat():
         # Install packages
-        os.system("sudo yum install bind bind-utils -y > /dev/null 2>&1")
+        os.system("yum install bind bind-utils -y > /dev/null 2>&1")
         # Creat bind9 install verify file
-        os.system("sudo touch /usr/share/bindbot/.bind9")
+        os.system("touch /usr/share/bindbot/.bind9")
         # Save service name
-        os.system("sudo echo 'named' | sudo tee -a /usr/share/bindbot/.service > /dev/null 2>&1")
+        os.system("echo 'named' | sudo tee -a /usr/share/bindbot/.service > /dev/null 2>&1")
         # Save config path
-        os.system("sudo echo '/var/named/' | sudo tee -a /usr/share/bindbot/.etc > /dev/null 2>&1")
+        os.system("echo '/var/named/' | sudo tee -a /usr/share/bindbot/.etc > /dev/null 2>&1")
         # Open ports
         print(bcolors.BOLD + "Opening ports..." + bcolors.ENDC)
-        os.system("sudo firewall-cmd --zone=public --add-port=53/tcp --permanent > /dev/null 2>&1")
-        os.system("sudo firewall-cmd --zone=public --add-port=53/tcp --permanent > /dev/null 2>&1")
-        os.system("sudo firewall-cmd --reload > /dev/null 2>&1")
+        os.system("firewall-cmd --zone=public --add-port=53/tcp --permanent > /dev/null 2>&1")
+        os.system("firewall-cmd --zone=public --add-port=53/tcp --permanent > /dev/null 2>&1")
+        os.system("firewall-cmd --reload > /dev/null 2>&1")
         # Enable service
         print(bcolors.BOLD + "Enable named service..." + bcolors.ENDC)
-        os.system("sudo systemctl enable named > /dev/null 2>&1")
+        os.system("systemctl enable named > /dev/null 2>&1")
 
         print(bcolors.OKGREEN + "Bind9 installation finished." + bcolors.ENDC)
 def installBind9():
@@ -65,8 +65,12 @@ def installBind9():
         os._exit(0)
 
 ## Start
+# Check root
+user = os.getenv("SUDO_USER")
+if user is None:
+    print(bcolors.FAIL + "This program need sudo."+ bcolors.ENDC)
+    exit()
 print(bcolors.HEADER + "BindBot 2.0 Beta" + bcolors.ENDC)
-
 # Check bind9 installed
 installed = os.path.exists("/usr/share/bindbot/.bind9")
 if (installed == False):
