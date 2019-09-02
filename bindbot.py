54b74eca3733b453ec315d5a5e9d3db09c5068f1
Amirhossein Bozorgiyan <amirhossein@bozorgiyan.ir>
2019-09-02 11:25:43 +0430

    Add 2>&1 and some messages in installDebian.

--- bindbot.py
+++ bindbot.py
@@ -4,22 +4,26 @@ import os
 ## Functions
 def installDebian():
     # Install packages
-    os.system("sudo apt install bind9 -y > /dev/null")
+    os.system("sudo apt install bind9 -y > /dev/null 2>&1")
     # Creat bind9 install verify file
     os.system("sudo touch /usr/share/bindbot/.bind9")
     # Save service name
-    os.system("sudo echo 'bind9' | sudo tee -a /usr/share/bindbot/.service > /dev/null")
+    os.system("sudo echo 'bind9' | sudo tee -a /usr/share/bindbot/.service > /dev/null 2>&1")
     # Save config path
-    os.system("sudo echo '/etc/bind/' | sudo tee -a /usr/share/bindbot/.etc > /dev/null")
+    os.system("sudo echo '/etc/bind/' | sudo tee -a /usr/share/bindbot/.etc > /dev/null 2>&1")
     # Open ports
-    os.system("sudo ufw allow 53/tcp > /dev/null")
-    os.system("sudo ufw allow 53/udp > /dev/null")
-    os.system("sudo iptables --append INPUT --match tcp --protocol tcp --sport 53 --jump ACCEPT > /dev/null")
-    os.system("sudo iptables --append OUTPUT --match tcp --protocol tcp --sport 53 --jump ACCEPT > /dev/null")
-    os.system("sudo iptables --append INPUT --match udp --protocol udp --sport 53 --jump ACCEPT > /dev/null")
-    os.system("sudo iptables --append OUTPUT --match udp --protocol udp --sport 53 --jump ACCEPT > /dev/null")
+    print("Opening ports...")
+    os.system("sudo ufw allow 53/tcp > /dev/null 2>&1")
+    os.system("sudo ufw allow 53/udp > /dev/null 2>&1")
+    os.system("sudo iptables --append INPUT --match tcp --protocol tcp --sport 53 --jump ACCEPT > /dev/null 2>&1")
+    os.system("sudo iptables --append OUTPUT --match tcp --protocol tcp --sport 53 --jump ACCEPT > /dev/null 2>&1")
+    os.system("sudo iptables --append INPUT --match udp --protocol udp --sport 53 --jump ACCEPT > /dev/null 2>&1")
+    os.system("sudo iptables --append OUTPUT --match udp --protocol udp --sport 53 --jump ACCEPT > /dev/null 2>&1")
     # Enable service
-    os.system("sudo systemctl enable bind9 > /dev/null")
+    print("Enable bind9 service...")
+    os.system("sudo systemctl enable bind9 > /dev/null 2>&1")
+
+    print("Bind9 installation finished.")
 
 def installBind9():
     debian = os.path.exists("/bin/apt")
